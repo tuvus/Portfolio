c6c82499d8dff5994909155352d33c0bc445ad85
Oskar Niesen <oskarniesen1@gmail.com>
2026-03-31 11:17:59 -0500

    Temporarily removed email

--- src/App.tsx
+++ src/App.tsx
@@ -80,16 +80,16 @@ function App() {
                             </g>
                         </svg>
                     </a>
-                    <a href="" target="_blank" className="duration-300 hover:scale-110">
-                        <svg width="34" height="40" fill="#ffffff" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 9 40 40"
-                             stroke="#ffffff" strokeWidth="0.00042">
-                            <g id="SVGRepo_iconCarrier">
-                                <path
-                                    d="M40.5,31.5v-18c0,0-18.2,12.7-19.97,13.359C18.79,26.23,0.5,13.5,0.5,13.5v18c0,2.5,0.53,3,3,3h34 C40.029,34.5,40.5,34.061,40.5,31.5z M40.471,9.971c0-1.821-0.531-2.471-2.971-2.471h-34c-2.51,0-3,0.78-3,2.6l0.03,0.28 c0,0,18.069,12.44,20,13.12C22.57,22.71,40.5,10.1,40.5,10.1L40.471,9.971z"></path>
-                            </g>
-                        </svg>
-                    </a>
+                    {/*<a href="" target="_blank" className="duration-300 hover:scale-110">*/}
+                    {/*    <svg width="34" height="40" fill="#ffffff" xmlns="http://www.w3.org/2000/svg"*/}
+                    {/*         viewBox="0 9 40 40"*/}
+                    {/*         stroke="#ffffff" strokeWidth="0.00042">*/}
+                    {/*        <g id="SVGRepo_iconCarrier">*/}
+                    {/*            <path*/}
+                    {/*                d="M40.5,31.5v-18c0,0-18.2,12.7-19.97,13.359C18.79,26.23,0.5,13.5,0.5,13.5v18c0,2.5,0.53,3,3,3h34 C40.029,34.5,40.5,34.061,40.5,31.5z M40.471,9.971c0-1.821-0.531-2.471-2.971-2.471h-34c-2.51,0-3,0.78-3,2.6l0.03,0.28 c0,0,18.069,12.44,20,13.12C22.57,22.71,40.5,10.1,40.5,10.1L40.471,9.971z"></path>*/}
+                    {/*        </g>*/}
+                    {/*    </svg>*/}
+                    {/*</a>*/}
                 </div>
 
             </header>
